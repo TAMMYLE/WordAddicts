96db1c7d5c5d8a74d6d3e2208d6b270eb8215efa
haphongle <hphongl93@gmail.com>
2019-04-16 17:26:27 +1000

    fixed an error for check button

--- app/src/main/java/com/example/wordaddicts/LettersStorage.java
+++ app/src/main/java/com/example/wordaddicts/LettersStorage.java
@@ -75,7 +75,7 @@ public class LettersStorage {
         char b[] = word2.toCharArray();
 
         boolean match = true;
-        if(a.length != b.length || a.length == 0 || b.length == 0)
+        if(a.length != b.length || a.length == 0 || b.length == 0 || a != null || a != null)
         {
             match = false;
             return match;
